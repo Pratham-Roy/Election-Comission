797bec077fd3b0ef5b0ed1074c089c746da06f4b
unknown <2k22.cse.2213369@gmail.com>
2025-03-06 18:09:17 +0530

    intial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Footer from "./Footer.jsx";
+import Header from "./header.jsx";
+
+function App() {
+  return (
+    <div className="hello">
+       <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import './header.css'
+function Header() {
+  return <div className="cherry"><h1 id="fh1">Eternals King's Society </h1> <h1 class="fh2">of Backchods and fellow Bhosdiwalas</h1></div>;
+}
+
+export default Header;
