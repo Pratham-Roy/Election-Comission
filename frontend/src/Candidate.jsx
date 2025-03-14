d801448a7ba2f2a54eee0ac168d830c6a9ed46dd
unknown <2k22.cse.2213369@gmail.com>
2025-03-14 14:50:14 +0530

    fourth commit

--- frontend/src/Candidate.jsx
+++ frontend/src/Candidate.jsx
@@ -1,15 +1,19 @@
 import './Candidate.css';
-import shaileshKumar from './assets/ShaileshKumar.jpg';
+import shaileshKumar from './assets/ShaileshKumar.jpg'; 
+import characters from './assets/candi.json'; 
 
 function Candidate() {
     return (
         <div className='candiboard'>
-            <div className="umeedwar">
-                <h2 className='candiname'>Shailesh Kumar</h2>
-                <img src={shaileshKumar} alt="Shailesh Kumar"  className="photo"/>
-                <p>Jab Shailesh bhai honge charche me. Russian peloge bin kharche me.</p>
-                <button className='voting'>Vote Me</button>
-            </div>
+            {characters.map((character) => (
+                <div className="umeedwar" key={character.id}>
+                    <h2 className='candiname'>{character.name}</h2>
+                    <img src={character.img || shaileshKumar} alt={character.name} className="photo" />
+                    <p>{character.slogan}</p>
+                    <p>{character.powers}</p>
+                    <button className='voting'>Vote Me</button>
+                </div>
+            ))}
         </div>
     );
 }
